2dcc4a6539ba78290d6a2b2f0de3f2d26bbae422
darwme <darw.mendz@gmail.com>
2024-06-19 21:13:31 -0500

    json object

---
+++ v2/server/src/app/pages/test/test-info.js
@@ -0,0 +1,28 @@
+{
+  "ISRA",
+    [
+      "opciones",
+      {
+        "casi nunca": 0,
+        "pocas veces": 1,
+        "unas veces sí, otras veces no": 2,
+        "muchas veces": 3,
+        "casi siempre": 4,
+      },
+      "secciones",
+      [
+        "cognitivo",
+        [
+          "situaciones",
+          {
+            1: "Ante un examen en el que me juego mucho, o si voy a ser entrevistado para un trabajo importante",
+            2: "",
+          },
+        ],
+        "fisiologico",
+        [],
+        "motor",
+        [],
+      ],
+    ];
+}
