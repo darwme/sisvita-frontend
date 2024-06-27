a32a98cb285d90415c082680ca61530258aa952a
Diego CH <158114096+diego-ch-21@users.noreply.github.com>
2024-06-26 20:36:16 -0500

    prueba perfil

--- v2/server/src/app/components/profile/profile.component.ts
+++ v2/server/src/app/components/profile/profile.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { DeacoderService } from '../../services/deacoder.service';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css',
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  type?: string;
+  personaType?: string;
+  dataDecrypt?: any;
+  user?: any;
+
+  constructor(private router: Router, private deacoder: DeacoderService) {
+    this.onDecrypt();
+  }
+
+  ngOnInit() {
+    this.onDecrypt();
+  }
+
+  onDecrypt() {
+    const encryptedData = localStorage.getItem('data');
+    if (encryptedData) {
+      this.dataDecrypt = this.deacoder.decrypt(encryptedData);
+    }
+
+    this.type = localStorage.getItem('userType') || undefined;
+    this.user = this.dataDecrypt;
+
+    if (!this.type) {
+      console.log('No userType found in localStorage');
+    } else {
+      this.personaType = localStorage.getItem('personaType') || undefined;
+      if (!this.personaType) {
+        this.router.navigate(['/auth/logout']);
+      } else if (this.personaType === 'paciente') {
+        this.user = this.user.paciente;
+      } else if (this.personaType === 'especialista') {
+        this.user = this.user.especialista;
+      } else {
+        this.user = this.user.administrador;
+      }
+    }
+
+    console.log('User:', this.user);
+  }
+}
